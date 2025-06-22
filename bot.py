71ad0ba8ed3fbf8edd6f2b70761815128ce83fb5
infernotrades <dking_1991@yahoo.com>
2025-06-21 22:06:55 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,44 @@
+import logging
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    filters,
+)
+from telegram import Update
+from handlers import (
+    start_command,
+    handle_callback_query,
+    handle_text,
+)
+
 import os
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
-from handlers import start_command, handle_callback_query, handle_text
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+async def error_handler(update: object, context: Exception):
+    logger.error(msg="Exception while handling an update:", exc_info=context)
 
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+    app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CallbackQueryHandler(handle_callback_query))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
 
-    print("🤖 Bot is running...")
+    app.add_error_handler(error_handler)
+
+    print("✅ Bot is running...")
     app.run_polling()
 
 if __name__ == "__main__":
