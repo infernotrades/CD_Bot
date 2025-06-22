277ed8429428fca9faae90e580d746392e8c0cde
infernotrades <dking_1991@yahoo.com>
2025-06-21 23:31:46 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ def main():
     app.add_error_handler(error_handler)
 
     print("✅ Bot is running...")
-    app.run_polling()
+    app.run_polling(allowed_updates=Update.ALL_TYPES, listen="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     main()
